07f94aa4b7a8d666ca1754a2e9e1ee17d2b51bcc
Nikhila Narayan <niki.narain@gmail.com>
2023-05-31 18:59:59 +0200

    background  image

--- src/app/movies/movies.page.spec.ts
+++ src/app/movies/movies.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { MoviesPage } from './movies.page';
 
 describe('MoviesPage', () => {
